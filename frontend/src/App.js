c4e141c1fbe7d9ff61ff740f24c82f73109dd3d5
MayaraSs <may_stein@hotmail.com>
2020-03-28 11:14:58 -0300

    creating the header

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,9 @@
 import React from "react";
 
+import Header from "./Header";
+
 function App() {
-  return <h1>Hello World</h1>;
+  return <Header>Semana OmniStack</Header>;
 }
 
 export default App;
